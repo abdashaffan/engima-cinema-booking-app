6997219ae8765c66b7d74492ea013720af292763
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-09-23 15:04:14 +0700

    debug direct function

--- core/Router.php
+++ core/Router.php
@@ -35,7 +35,7 @@ class Router
     public function direct($uri, $requestType)
     {
         if (array_key_exists($uri, $this->routes[$requestType])) {
-            return $this->routes[$uri];
+            return $this->routes[$requestType][$uri];
         }
 
         throw new Exception('No route defined for this URI.');
