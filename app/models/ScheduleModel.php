ca507e124c1d205ae1587cca3aae8d30aa661a7f
Rika Dewi <rika_4444@yahoo.com>
2019-09-28 23:10:45 +0700

    Add seat view

---
+++ app/models/ScheduleModel.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class ScheduleModel
+{
+    private $table = 'schedule';
+    private $db;
+
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    //TODO: ubah jadi yg blom lewat hari ini
+    public function getAllScheduleByFilmId($id)
+    {   
+        $this->db->query("SELECT {$this->table}.schedule_id, showtime, count FROM {$this->table}  LEFT JOIN (SELECT schedule_id, COUNT(*) AS count FROM seats GROUP BY(schedule_id)) as count_seat ON {$this->table}.schedule_id=count_seat.schedule_id WHERE film_id={$id}"
+        );
+        return $this->db->resultSet();
+    }
+}
---
+++ app/views/seat/index.view.php
@@ -0,0 +1,29 @@
+<div class="seat">
+    <div class="seat-detail">
+        <img class="svg-superbig" src="/public/assets/icon/chevron-left-solid.svg">
+        <div>
+            <h3>Avengers: Endgame</h3>
+            <h4>September 4, 2019 - 09.40 PM</h4>
+        </div>
+    </div>
+    <div class="seat-desc">
+        <div class="seat-number-wrapper">
+            <div class="seat-number">
+                <?php for ($i = 1; $i<=30; $i++ ): ?>
+                    <div>
+                        <?php echo $i; ?>
+                    </div>
+                <?php endfor ?>
+            </div>
+            <div class="seat-screen">
+                Screen
+            </div>
+        </div>
+        <div class="seat-summary"> 
+            <h4>Booking Summary</h4>
+            <p>
+                You haven't selected any seat yet. Please click on one of the seat provided.
+            </p>
+        </div>
+    </div>
+</div>
