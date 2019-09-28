ee95bdc303e4d222a19929c74ae360054bb1252a
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-09-28 11:25:03 +0700

    edit register

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -13,11 +13,29 @@ class RegisterController extends Controller
     public function index()
     {
 
+        var_dump("Register/index");
         $data['judul'] = 'Register/index';
         $data['css'] = $this->cssPath . "/style.css";
         $data['js'] = $this->jsPath . "/index.js";
         $this->view('templates/header', $data);
         $this->view('register/index', $data);
-        $this->view('templates/footer');
+        $this->view('templates/footer', $data);
+    }
+
+    public function validateUsername()
+    {
+        var_dump($_GET['username']);
+        $data['username'] = $_GET['username'];
+        $data['css'] = $this->cssPath . "/style.css";
+        $data['js'] = $this->jsPath;
+
+
+        if (!$this->model('Register')->checkIfUsernameExist($data['username'])) {
+            $data["usernameExistMsg"] = "Username {$data["username"]} is already exist!";
+        }
+
+        $this->view('templates/header', $data);
+        $this->view('register/index', $data);
+        $this->view('templates/footer', $data);
     }
 }
