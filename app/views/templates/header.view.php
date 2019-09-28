963c82fd986737a18dcbd9ba95bc4dcc6b2d9095
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-09-28 11:48:50 +0700

    merge home component

--- app/views/templates/header.view.php
+++ app/views/templates/header.view.php
@@ -5,8 +5,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" type="text/css" href="<?= BASE_CSS; ?>" />
-        <link rel="stylesheet" type="text/css" href="<?= $css; ?>" />
+        <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+            integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?= BASE_CSS; ?>">
+        <link rel="stylesheet" href="<?= $css; ?>">
         <title><?= $judul; ?></title>
     </head>
     <!-- <nav style="display:block;">
