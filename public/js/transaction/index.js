b9aea43b2c5bced3387a4f2aa9a7a082f6dc0082
unknown <xeldkt@gmail.com>
2019-09-29 13:16:43 +0700

    index

--- public/js/transaction/index.js
+++ public/js/transaction/index.js
@@ -1,13 +1,7 @@
 
 
 const deleteReview = (identifier) => {
-    //1. Jalanin get pair review id
-    //2. delete review by review id (in reviewModel) and update status
-    //3. refresh
 }
 
 const editReview = (identifier) => {
-    //1. Jalanin get pair review id
-    //2. same as add
-    //3. Edit instead of Add
 }
