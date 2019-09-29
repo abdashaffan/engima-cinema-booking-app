09df73f541c53d1949960aa1d1dcbafa44b562c7
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-09-29 09:17:29 +0700

    debug login

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
         parent::__construct('login');
     }
 
-    public function index()
+    public function index($valid = 1)
     {
 
         if ($this->model("Login")->isRedirectedToHome()) {
