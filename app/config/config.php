72f61d8b39e0ba48a49f5452f8f466062d08cc1c
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-11-18 19:06:28 +0700

    debug config and search

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('PROJECT_NAME', 'tugas-besar-1-2019');
+define('PROJECT_NAME', 'engima');
 define('ROOT',  "{$_SERVER["DOCUMENT_ROOT"]}/" . PROJECT_NAME);
 define('DEFAULT_CONTROLLER', 'HomeController');
 define('DEFAULT_METHOD', 'index');
@@ -9,8 +9,8 @@ define('DEFAULT_METHOD', 'index');
 define('BASE_URL', 'http://localhost/' . PROJECT_NAME . '/public');
 // GANTI SESUAI CONFIG MYSQL MASING"
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'tubes_wbd');
+define('DB_USER', 'abda');
+define('DB_PASS', 'abda');
+define('DB_NAME', 'tubes_wbd_1');
 define('BASE_CSS', "/" . PROJECT_NAME . "/public/css/base.css");
 define('BASE_JS', "/" . PROJECT_NAME . "/public/js/index.js");
--- public/js/search/index.js
+++ public/js/search/index.js
@@ -1,18 +1,19 @@
-const  scrollWin = () => {
-    window.scrollTo(500, 0);
-  }
-const loadData = (page=1) => {
-    let keyword = document.querySelector('.keyword').value;
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let searchResult = JSON.parse(this.responseText);
-            document.querySelector('.container').innerHTML =  searchResult.output;
-        }
-    };
-    xmlhttp.open("POST", BASE_URL + "/search/result", true);
-    xmlhttp.setRequestHeader('Content-Type', 'application/json');
-    xmlhttp.send(JSON.stringify({page,keyword}));
-}
+const scrollWin = () => {
+  window.scrollTo(500, 0);
+};
+const loadData = (page = 1) => {
+  let keyword = document.querySelector(".keyword").value;
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let searchResult = JSON.parse(this.responseText);
+      document.querySelector(".container").innerHTML = searchResult.output;
+    }
+  };
+  xmlhttp.open("POST", BASE_URL + "/search/result", true);
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  console.log(JSON.stringify({ page, keyword }));
+  xmlhttp.send(JSON.stringify({ page, keyword }));
+};
 
 loadData();
