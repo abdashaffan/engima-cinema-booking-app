4ae4e112e6ab499184f7ece11852a3a4efcdfc8a
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-11-22 09:41:11 +0700

    add config

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+define('PROJECT_NAME', 'engima');
+define('ROOT',  "{$_SERVER["DOCUMENT_ROOT"]}/" . PROJECT_NAME);
+define('DEFAULT_CONTROLLER', 'HomeController');
+define('DEFAULT_METHOD', 'index');
+// GANTI SESUAI NAMA FILE MASING"
+define('BASE_URL', 'http://localhost/' . PROJECT_NAME . '/public');
+// GANTI SESUAI CONFIG MYSQL MASING"
+define('DB_HOST', 'localhost');
+define('DB_USER', 'abda');
+define('DB_PASS', 'abda');
+define('DB_NAME', 'tubes_wbd_1');
+define('BASE_CSS', "/" . PROJECT_NAME . "/public/css/base.css");
+define('BASE_JS', "/" . PROJECT_NAME . "/public/js/index.js");
+define('TRANSACTION_WS_URL', 'http://localhost:3000');
+define('BANK_WS_URL', 'localhost:8080');
