ec5cc19037cdc9ee9bab69fede1a5e63f2f8fdaa
Alam <13517096@std.stei.itb.ac.id>
2019-11-24 18:09:50 +0700

    BASE_URL changed to amazon

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,7 @@ define('ROOT',  "{$_SERVER["DOCUMENT_ROOT"]}/" . PROJECT_NAME);
 define('DEFAULT_CONTROLLER', 'HomeController');
 define('DEFAULT_METHOD', 'index');
 // GANTI SESUAI NAMA FILE MASING"
-define('BASE_URL', 'http://localhost/' . PROJECT_NAME . '/public');
+define('BASE_URL', 'http://ec2-107-21-68-151.compute-1.amazonaws.com/' . PROJECT_NAME . '/public');
 // GANTI SESUAI CONFIG MYSQL MASING"
 define('DB_HOST', 'database-1.cmqcolum2oyw.us-east-1.rds.amazonaws.com');
 define('DB_USER', 'rootroot');
